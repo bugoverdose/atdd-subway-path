26a2be47a01bae589eef75c494b7fc831f2a2d9c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-26 21:38:57 +0900

    refactor(domain): cleanse constants at AgeDiscountPolicy

--- src/main/java/wooteco/subway/domain/fare/AgeDiscountPolicy.java
+++ src/main/java/wooteco/subway/domain/fare/AgeDiscountPolicy.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public enum AgeDiscountPolicy {
 
-    BABY(0, Constants.CHILD_MIN_AGE, 0, 1.00),
-    CHILD(Constants.CHILD_MIN_AGE, Constants.ADOLESCENT_MIN_AGE, 350, 0.50),
-    ADOLESCENT(Constants.ADOLESCENT_MIN_AGE, Constants.ADULT_MIN_AGE, 350, 0.20),
-    ADULT(Constants.ADULT_MIN_AGE, Constants.ELDERLY_MIN_AGE, 0, 0.00),
-    ELDERLY(Constants.ELDERLY_MIN_AGE, 150, 0, 1.00),
+    BABY(Age.MIN, Age.CHILD_MIN, 0, 1.00),
+    CHILD(Age.CHILD_MIN, Age.ADOLESCENT_MIN, 350, 0.50),
+    ADOLESCENT(Age.ADOLESCENT_MIN, Age.ADULT_MIN, 350, 0.20),
+    ADULT(Age.ADULT_MIN, Age.ELDERLY_MIN, 0, 0.00),
+    ELDERLY(Age.ELDERLY_MIN, Age.MAX, 0, 1.00),
     ;
 
-    private static final String INVALID_AGE_RANGE_EXCEPTION = "0과 150 사이의 연령만 입력가능합니다.";
+    private static final String INVALID_AGE_RANGE_EXCEPTION =
+            String.format("%d과 %d 사이의 연령만 입력가능합니다.", Age.MIN, Age.MAX);
 
     private final int startInclusive;
     private final int endExclusive;
@@ -40,11 +41,13 @@ public enum AgeDiscountPolicy {
         return (int) ((fare - discountAmount) * (1 - discountRatio));
     }
 
-    private static class Constants {
+    private static class Age {
 
-        static final int CHILD_MIN_AGE = 6;
-        static final int ADOLESCENT_MIN_AGE = 13;
-        static final int ADULT_MIN_AGE = 19;
-        static final int ELDERLY_MIN_AGE = 65;
+        static final int MIN = 0;
+        static final int CHILD_MIN = 6;
+        static final int ADOLESCENT_MIN = 13;
+        static final int ADULT_MIN = 19;
+        static final int ELDERLY_MIN = 65;
+        static final int MAX = 150;
     }
 }
